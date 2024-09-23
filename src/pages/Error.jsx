10a9b02fd74f67daf092262ad65797f6d04a24ff
Obayuwana22 <successobayuwana@gmail.com>
2024-09-23 17:59:37 +0100

    New commit

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,6 +1,9 @@
-    const Error = () => {
-      return (
-        <div>Error</div>
-      )
-    }
-    export default Error
+const Error = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+export default Error;
